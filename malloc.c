8bfe97a0494e97bc077a0322fd429fb0e274e2ee
Corey Henderson <corman@cormander.com>
2011-07-03 02:43:09 -0600

    Use .run on malloc sym structs

--- malloc.c
+++ malloc.c
@@ -1,9 +1,6 @@
 
 #include "tpe.h"
 
-void *(*module_alloc_func)(unsigned long) = NULL;
-void (*module_free_func)(struct module *, void *) = NULL;
-
 static struct kernsym sym_module_alloc;
 static struct kernsym sym_module_free;
 
@@ -23,43 +20,26 @@ int malloc_init(void) {
 	if (IS_ERR(ret))
 		return ret;
 	
-	module_alloc_func = (void *(*)(unsigned long))sym_module_alloc.addr;
-	module_free_func = (void (*)(struct module *, void *))sym_module_free.addr;
-
-	if (module_alloc_func == NULL) {
-		printk(KERN_ERR "[tpe] "
-			"Unable to find \"module_alloc\" function\n");
-		return -EFAULT;
-	}
-	
-	if (module_free_func == NULL) {
-		printk(KERN_ERR "[tpe] "
-			"Unable to find \"module_free\" function\n");
-		return -EFAULT;
-	}
-		
 	return 0;
 }
 
 // "forget" about it
 
 void malloc_clean(void) {
-	module_alloc_func = NULL;
-	module_free_func = NULL;
+	symbol_restore(&sym_module_alloc);
+	symbol_restore(&sym_module_free);
 }
 
 // call to module_alloc
 
 void *malloc(unsigned long size) {
-	BUG_ON(module_alloc_func == NULL);
-	return module_alloc_func(size);
+	return sym_module_alloc.run(size);
 }
 
 // call to module_free
 
 void malloc_free(void *buf) {
-	BUG_ON(module_free_func == NULL);
 	if (buf != NULL)
-		module_free_func(NULL, buf);
+		sym_module_free.run(NULL, buf);
 }
 
