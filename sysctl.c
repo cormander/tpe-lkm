6383d14275a31ca57822d68af3ca8a25fa520eae
Corey Henderson <corman@cormander.com>
2012-04-26 19:43:05 -0600

    ctl_name doesn't exist in version >= 2.6.33

--- sysctl.c
+++ sysctl.c
@@ -22,7 +22,9 @@ int tpe_ps_gid = 0;
 
 static ctl_table tpe_extras_table[] = {
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "lsmod",
 		.data		= &tpe_lsmod,
 		.maxlen		= sizeof(int),
@@ -30,7 +32,9 @@ static ctl_table tpe_extras_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "ps",
 		.data		= &tpe_ps,
 		.maxlen		= sizeof(int),
@@ -38,7 +42,9 @@ static ctl_table tpe_extras_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "ps_gid",
 		.data		= &tpe_ps_gid,
 		.maxlen		= sizeof(int),
@@ -46,7 +52,9 @@ static ctl_table tpe_extras_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "proc_kallsyms",
 		.data		= &tpe_proc_kallsyms,
 		.maxlen		= sizeof(int),
@@ -58,7 +66,9 @@ static ctl_table tpe_extras_table[] = {
 
 static ctl_table tpe_table[] = {
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "softmode",
 		.data		= &tpe_softmode,
 		.maxlen		= sizeof(int),
@@ -66,7 +76,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "trusted_gid",
 		.data		= &tpe_trusted_gid,
 		.maxlen		= sizeof(int),
@@ -74,7 +86,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "admin_gid",
 		.data		= &tpe_admin_gid,
 		.maxlen		= sizeof(int),
@@ -82,7 +96,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "dmz_gid",
 		.data		= &tpe_dmz_gid,
 		.maxlen		= sizeof(int),
@@ -90,7 +106,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "strict",
 		.data		= &tpe_strict,
 		.maxlen		= sizeof(int),
@@ -98,7 +116,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "check_file",
 		.data		= &tpe_check_file,
 		.maxlen		= sizeof(int),
@@ -106,7 +126,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "paranoid",
 		.data		= &tpe_paranoid,
 		.maxlen		= sizeof(int),
@@ -114,16 +136,22 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
         {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
                 .ctl_name       = CTL_UNNUMBERED,
+#endif
                 .procname       = "hardcoded_path",
                 .data           = &tpe_hardcoded_path,
                 .maxlen         = TPE_HARDCODED_PATH_LEN,
                 .mode           = 0644,
                 .proc_handler   = &proc_dostring,
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
                 .strategy       = &sysctl_string,
+#endif
         },
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "kill",
 		.data		= &tpe_kill,
 		.maxlen		= sizeof(int),
@@ -131,7 +159,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "log",
 		.data		= &tpe_log,
 		.maxlen		= sizeof(int),
@@ -139,7 +169,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "log_max",
 		.data		= &tpe_log_max,
 		.maxlen		= sizeof(int),
@@ -147,7 +179,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "log_floodtime",
 		.data		= &tpe_log_floodtime,
 		.maxlen		= sizeof(int),
@@ -155,7 +189,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "log_floodburst",
 		.data		= &tpe_log_floodburst,
 		.maxlen		= sizeof(int),
@@ -163,7 +199,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "lock",
 		.data		= &tpe_lock,
 		.maxlen		= sizeof(int),
@@ -171,7 +209,9 @@ static ctl_table tpe_table[] = {
 		.proc_handler	= &proc_dointvec,
 	},
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= "extras",
 		.mode		= 0500,
 		.child		= tpe_extras_table,
@@ -181,7 +221,9 @@ static ctl_table tpe_table[] = {
 
 static ctl_table tpe_root_table[] = {
 	{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 33)
 		.ctl_name	= CTL_UNNUMBERED,
+#endif
 		.procname	= MODULE_NAME,
 		.mode		= 0500,
 		.child		= tpe_table,
