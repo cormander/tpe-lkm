a2e0a2fdfa1e432eb2ab0c9d49188bdf9797bd9e
Corey Henderson <corman@cormander.com>
2014-08-27 23:05:33 -0400

    Implement test for kill sysctl

--- tests/sysctl-kill.sh
+++ tests/sysctl-kill.sh
@@ -2,7 +2,20 @@
 
 uid=$1
 
-echo "Test not implemented yet"
+sudo -u "#$uid" cp /bin/true /tmp/tpe-tests
 
-exit -1
+sysctl tpe.kill=1
+
+sudo -u "#$uid" sh -c '/tmp/tpe-tests'
+
+if [ $? -ne 137 ]; then
+	echo "Exit status of sudo subshell wasn't 137"
+	ret=1
+fi
+
+sysctl tpe.kill=0
+
+rm -f /tmp/tpe-tests
+
+exit $ret
 
