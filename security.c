06437293e3ffe8cdd91b035fbb1b4468d25f134f
Corey Henderson <corman@cormander.com>
2017-03-23 21:15:14 -0400

    macro the HELL out of this file

--- security.c
+++ security.c
@@ -1,81 +1,56 @@
 
 #include "module.h"
 
-struct kernsym sym_security_bprm_check;
-struct kernsym sym_security_mmap_file;
-struct kernsym sym_security_file_mprotect;
-struct kernsym sym_m_show;
-
 int tpe_donotexec(void) {
 	return -EACCES;
 }
 
+#ifdef CONFIG_X86_64
+#define REGS_ARG1(r) r->di
+#define REGS_ARG2(r) r->si
+#define REGS_ARG3(r) r->dx
+#else
+#error "Arch not currently supported."
+#endif
+
+#define tpe_trace_handler(val) \
+	static void notrace tpe_##val(unsigned long ip, unsigned long parent_ip, \
+		struct ftrace_ops *fops, struct pt_regs *regs)
+
 // mmap
 
-static void notrace tpe_security_mmap_file(unsigned long ip, unsigned long parent_ip,
-		      struct ftrace_ops *fops, struct pt_regs *regs) {
-
-	//struct file *file = (struct file *)regs->di;
-	//unsigned long prot = (unsigned long)regs->si;
-
-	if (regs->di && (regs->si & PROT_EXEC))
-		if (tpe_allow_file((struct file *)regs->di, "mmap"))
+tpe_trace_handler(security_mmap_file) {
+	if (REGS_ARG1(regs) && (REGS_ARG2(regs) & PROT_EXEC))
+		if (tpe_allow_file((struct file *)REGS_ARG1(regs), "mmap"))
 			regs->ip = (unsigned long)tpe_donotexec;
 }
 
 // mprotect
 
-static void notrace tpe_security_file_mprotect(unsigned long ip, unsigned long parent_ip,
-			struct ftrace_ops *fops, struct pt_regs *regs) {
+tpe_trace_handler(security_file_mprotect) {
+	struct vm_area_struct *vma = (struct vm_area_struct *)REGS_ARG1(regs);
 
-	struct vm_area_struct *vma = (struct vm_area_struct *)regs->di;
-	//unsigned long prot = (unsigned long)regs->dx;
-
-	if (vma->vm_file && (regs->dx & PROT_EXEC))
+	if (vma->vm_file && (REGS_ARG2(regs) & PROT_EXEC))
 		if (tpe_allow_file(vma->vm_file, "mprotect"))
 			regs->ip = (unsigned long)tpe_donotexec;
 }
 
 // execve
 
-static void notrace tpe_security_bprm_check(unsigned long ip, unsigned long parent_ip,
-			struct ftrace_ops *fops, struct pt_regs *regs) {
-
-	struct linux_binprm *bprm = (struct linux_binprm *)regs->di;
+tpe_trace_handler(security_bprm_check) {
+	struct linux_binprm *bprm = (struct linux_binprm *)REGS_ARG1(regs);
 
 	if (bprm->file)
 		if (tpe_allow_file(bprm->file, "exec"))
 			regs->ip = (unsigned long)tpe_donotexec;
 }
 
-static void notrace tpe_m_show(unsigned long ip, unsigned long parent_ip,
-			struct ftrace_ops *fops, struct pt_regs *regs) {
+// lsmod
 
+tpe_trace_handler(m_show) {
 	if (tpe_lsmod && !capable(CAP_SYS_MODULE))
 		regs->ip = (unsigned long)tpe_donotexec;
 }
-
-#define printfail(str,ret) printk(PKPRE "warning: unable to implement protections for %s in %s() at line %d, return code %d\n", str, __FUNCTION__, __LINE__, ret)
-
-static struct ftrace_ops fops_security_mmap_file __read_mostly = {
-	.func = tpe_security_mmap_file,
-	.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY,
-};
-
-static struct ftrace_ops fops_security_file_mprotect __read_mostly = {
-	.func = tpe_security_file_mprotect,
-	.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY,
-};
-
-static struct ftrace_ops fops_security_bprm_check __read_mostly = {
-	.func = tpe_security_bprm_check,
-	.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY,
-};
-
-static struct ftrace_ops fops_m_show __read_mostly = {
-	.func = tpe_m_show,
-	.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY,
-};
 
 struct symhook {
 	char *name;
@@ -83,12 +58,31 @@ struct symhook {
 	struct ftrace_ops *fops;
 };
 
-// order matters for optimization
+#define struct_kernsym(val) struct kernsym sym_##val
+
+#define struct_ftrace_ops(val) \
+static struct ftrace_ops fops_##val __read_mostly = { \
+	.func = tpe_##val, \
+	.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY, \
+}
+
+#define tpe_make_structs(val) \
+	struct_kernsym(val); \
+	struct_ftrace_ops(val);
+
+tpe_make_structs(security_mmap_file);
+tpe_make_structs(security_file_mprotect);
+tpe_make_structs(security_bprm_check);
+tpe_make_structs(m_show);
+
+#define symhook_val(val) \
+	{#val, &sym_##val, &fops_##val}
+
 struct symhook security2hook[] = {
-	{"security_mmap_file", &sym_security_mmap_file, &fops_security_mmap_file},
-	{"security_file_mprotect", &sym_security_file_mprotect, &fops_security_file_mprotect},
-	{"security_bprm_check", &sym_security_bprm_check, &fops_security_bprm_check},
-	{"m_show", &sym_m_show, &fops_m_show},
+	symhook_val(security_mmap_file),
+	symhook_val(security_file_mprotect),
+	symhook_val(security_bprm_check),
+	symhook_val(m_show),
 };
 
 int symbol_ftrace(const char *symbol_name, struct kernsym *sym, struct ftrace_ops *fops) {
@@ -149,7 +143,7 @@ int symbol_restore(struct kernsym *sym, struct ftrace_ops *fops) {
 	return 0;
 }
 
-// ftrace the needed functions. whenever possible, ftrace just the LSM function
+#define printfail(str,ret) printk(PKPRE "warning: unable to implement protections for %s in %s() at line %d, return code %d\n", str, __FUNCTION__, __LINE__, ret)
 
 void ftrace_syscalls(void) {
 
@@ -169,6 +163,7 @@ void undo_ftrace_syscalls(void) {
 
 	for (i = 0; i < ARRAY_SIZE(security2hook); i++) {
 		ret = symbol_restore(security2hook[i].sym, security2hook[i].fops);
+
 		if (IN_ERR(ret))
 			printfail(security2hook[i].name, ret);
 	}
