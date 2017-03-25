ee65046703baaa2ebac4f9dc44f3b994fa9e8c37
Corey Henderson <corman@cormander.com>
2017-03-24 23:01:47 -0400

    Additional sanity check in hardcoded path test

--- tests/sysctl-hardcoded_path.sh
+++ tests/sysctl-hardcoded_path.sh
@@ -3,15 +3,18 @@
 uid=$1
 
 # build what is probably isn't a complete path of this system, but good enough for a test
-h_path=$PATH:$(for i in $(ps aux | awk '{print $11}' | grep '^/' | sort -u); do ldd $i 2> /dev/null; done | sort -u | awk '{print $3}' | sort -u | grep -v 0x | sed 's|/[^/]*$||' | sort -u | grep '^/' | tr '\n' ':' | sed 's/:$//')
+h_path=$PATH:/usr/lib64:$(for i in $(ps aux | awk '{print $11}' | grep '^/' | sort -u); do ldd $i 2> /dev/null; done | sort -u | awk '{print $3}' | sort -u | grep -v 0x | sed 's|/[^/]*$||' | sort -u | grep '^/' | tr '\n' ':' | sed 's/:$//')
 
 # some sanity checks on length
 len=$(echo $h_path | wc -m)
 
 if [ $len -lt 10 ] || [ $len -gt 1024 ]; then
 	# exit here, not safe to continue
+	echo "test \$h_path is too long."
 	exit 1
 fi
+
+echo PATH="$h_path"
 
 mkdir /notinthepath/
 
@@ -34,6 +37,15 @@ sudo -u "#$uid" /notinthepath/true
 if [ $? == 0 ]; then
         echo "Could execute /notinthepath/true"
         ret=1
+fi
+
+# this should work
+
+sudo -u "#$uid" /bin/true
+
+if [ $? != 0 ]; then
+	echo "Could NOT execute /bin/true"
+	ret=1
 fi
 
 echo "" > /proc/sys/tpe/hardcoded_path
