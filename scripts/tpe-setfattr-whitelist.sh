7a4b4867af7b30df1c6fc5aa4d6b69c22a69db0f
Corey Henderson <corman@cormander.com>
2017-03-31 22:25:21 -0400

    Expand whitelisting to syscalls other than mmap

--- scripts/tpe-setfattr-whitelist.sh
+++ scripts/tpe-setfattr-whitelist.sh
@@ -5,8 +5,13 @@ if [ ! -x /usr/bin/setfattr ]; then
 	exit 1
 fi
 
-cat /etc/sysconfig/tpe-whitelist | while read bin; do
-	[ ! -f "$bin" ] && continue
-	setfattr -n security.tpe -v "soften_mmap" "$bin"
+for syscall in mmap mprotect exec setuid lsmod ptrace uname; do
+
+	cat /etc/sysconfig/tpe-$syscall-whitelist 2> /dev/null | while read bin; do
+		[ ! -f "$bin" ] && continue
+		echo "tpe: whitelisting \"$bin\" for \"$syscall\" syscall"
+		setfattr -n security.tpe -v "soften_$syscall" "$bin"
+	done
+
 done
 
