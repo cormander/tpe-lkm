ded8afcb2d0988e37c91dcc89906036a238fd673
Corey Henderson <corman@cormander.com>
2017-04-02 12:36:54 -0400

    Tag version 2.0.1

--- tpe_module.c
+++ tpe_module.c
@@ -210,5 +210,5 @@ module_exit(tpe_exit);
 MODULE_AUTHOR("Corey Henderson");
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Trusted Path Execution (TPE) Module");
-MODULE_VERSION("2.0.0");
+MODULE_VERSION("2.0.1");
 
