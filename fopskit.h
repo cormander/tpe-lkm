1a07a26624b186afb6cb9ec0e5b8e564da279454
Corey Henderson <corman@cormander.com>
2017-04-11 01:02:43 -0400

    actually return in fopskit_return

--- fopskit.h
+++ fopskit.h
@@ -19,7 +19,7 @@ struct fops_hook {
 	struct ftrace_ops *fops;
 };
 
-#define fopskit_return(func) regs->ip = (unsigned long)func;
+#define fopskit_return(func) regs->ip = (unsigned long)func; return;
 
 #define fops_hook_val(val) \
 	{#val, NULL, false, false, &fops_##val}
