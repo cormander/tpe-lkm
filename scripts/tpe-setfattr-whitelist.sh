279dbc67015ecb0b9a21977173a323a1e11b5ddd
Corey Henderson <corman@cormander.com>
2017-05-06 12:00:16 -0400

    don't erase pre-existing soften flags

--- scripts/tpe-setfattr-whitelist.sh
+++ scripts/tpe-setfattr-whitelist.sh
@@ -5,12 +5,24 @@ if [ ! -x /usr/bin/setfattr ]; then
 	exit 1
 fi
 
+if [ ! -x /usr/bin/getfattr ]; then
+	echo "Could not find /usr/bin/getfattr"
+	exit 1
+fi
+
 for syscall in mmap mprotect exec setuid lsmod ptrace uname; do
 
 	cat /etc/sysconfig/tpe-$syscall-whitelist 2> /dev/null | while read bin; do
 		[ ! -f "$bin" ] && continue
 		echo "tpe: whitelisting \"$bin\" for \"$syscall\" syscall"
-		setfattr -n security.tpe -v "soften_$syscall" "$bin"
+
+		# first get what soften_X may already exist, minus this one
+		flags=$(getfattr -n security.tpe $bin 2> /dev/null | grep security.tpe | cut -d '"' -f 2 | sed "s/soften_$syscall//")
+
+		# ensure no stray commas
+		new_flags=$(echo "$flags,soften_$syscall" | sed 's/^,//' | sed 's/,$//')
+
+		setfattr -n security.tpe -v "$new_flags" "$bin"
 	done
 
 done
